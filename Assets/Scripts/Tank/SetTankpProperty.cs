22760acd894cee40ea6b1756b06c85dd0d688b3d
czy <chenzhiyang@haishengyue.com>
2016-01-05 17:55:37 +0800

    增加ui的效果

---
+++ Assets/Scripts/Tank/SetTankpProperty.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections;
+
+public class SetTankpProperty : MonoBehaviour {
+	public ToggleGroup colorselect;
+	public InputField nameinput;
+	public Material tankcolor;
+	public Text Name;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		IEnumerator<Toggle> toggleEnum = colorselect.ActiveToggles ().GetEnumerator ();
+		toggleEnum.MoveNext ();
+		Toggle toggle = toggleEnum.Current;	
+		switch (toggle.name) {
+		case "OptionA":
+			tankcolor.color = Color.red;
+		break;
+		case "OptionB":
+			tankcolor.color = Color.black;
+		break;
+		case "OptionC":
+			tankcolor.color = Color.blue;
+		break;
+		}
+		Name.text = nameinput.text;
+	}
+
+	public void StartGame()
+	{
+		//TODO
+	}
+}
